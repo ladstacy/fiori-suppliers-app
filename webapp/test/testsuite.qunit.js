e947c3407de2c23036dae0c619d502b47d09338f
Anastassiya Litvichenko <a.litvichenko@LeverX.local>
2025-02-07 17:39:44 +0300

    Initial commit

---
+++ webapp/test/testsuite.qunit.js
@@ -0,0 +1,10 @@
+sap.ui.define(() => {
+    "use strict";
+    return {
+        tests: {
+            "integration/opaTests": {
+                title: "UI5 Walkthrough - Integration Tests"
+            }
+        }
+    };
+});
